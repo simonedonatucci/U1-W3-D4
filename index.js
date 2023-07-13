c9ca93c309bd162e4da72c473a5685b7b5968cc7
simonedonatucci <donatucci.simone99@gmail.com>
2023-07-13 16:56:02 +0200

    U1-W3-D4

---
+++ index.js
@@ -0,0 +1,12 @@
+const Array = function () {
+  const arr = [];
+  for (let i = 0; i < 76; i++) {
+    arr.push(i + 1);
+  }
+  return arr;
+};
+const randomNumber = function (casual) {
+  const randonNUm = Math.floor(Math.random() * casual.length);
+  const randomSplice = range.splice(randIndex, 1)[0];
+  return random;
+};
